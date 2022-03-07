314b266afc7d43e7c2027239a46a683c0c1eabd3
TwelveCat-G3 <ebola52012@gmail.com>
2022-03-07 13:27:45 +0800

    Revert "发现因为‘onBrokenLinks’导致的报错，现在修改了报错级别"
    This reverts commit 9eef404408e6a3036bd5c426ef0876b4f2751ad1.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,9 +8,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: "TwelveCat's Wiki",   // 站点名称
   tagline: '^_^',  // 站点描述
-  url: 'https://github.com', // GitHub 页面的 URL
-  baseUrl: '/Twelvecat/Wiki_Docusaurus/',
-  onBrokenLinks: 'warn',  //这个是检测链接是否失效的，在这个地方报了bug
+  url: 'https://github.com/Twelvecat', // GitHub 页面的 URL
+  baseUrl: '/Wiki_Docusaurus/',
+  onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico', // 站点的图标
   organizationName: 'Twelvecat', // Usually your GitHub org/user name.
