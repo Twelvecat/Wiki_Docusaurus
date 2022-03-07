4a35117b1e64b74dd0eda2fe572513cf0db5a77d
TwelveCat-G3 <ebola52012@gmail.com>
2022-03-07 13:29:46 +0800

    Revert "修复失败了emm，再修复一次"
    This reverts commit bbec63e3bb7752ed8818deb3c9194d853894ce78.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: "TwelveCat's Wiki",   // 站点名称
   tagline: '^_^',  // 站点描述
-  url: 'https://github.com/Twelvecat', // GitHub 页面的 URL
-  baseUrl: '/Wiki_Docusaurus/',
+  url: 'https://wiki-docusaurus-blue.vercel.app', // GitHub 页面的 URL
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico', // 站点的图标
